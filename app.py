69d071243f141dd9949271c5780a6574ea620c28
Yusuf A <yusuf.r.ahmed@proton.me>
2026-09-06 16:04:21 +0100

    added flask template, used template in app.py, created a post method in html which syncs with sql lite db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import sqlite3
+import feedparser
 
 def connect_db():
     dbcon = sqlite3.connect("rss.db")
@@ -8,18 +9,31 @@ def connect_db():
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     dbcon = connect_db()
+
+    #when button on website is pressed code below is executed 
+    if request.method == "POST":
+        template_name = request.form["name"]
+        headers = request.form["headers"]
+
+        #inserts into database (?,?) unnamed parameters prevents sql injection
+        dbcon.execute(
+                "INSERT INTO feed_templates (name, headers) VALUES (?, ?)",
+                (template_name, headers))
+        dbcon.commit()
+        dbcon.close()
+
+    
+        # clean get request refresh
+        return redirect("/")
+
     templates = dbcon.execute("SELECT * FROM feed_templates").fetchall()
     dbcon.close()
 
-    #bulding a html list from db
-    html = "<ul>"
-    for entry in templates:
-        html += f"<li>{entry["name"]} headers = {entry["headers"]}"
-    html += "</ul>"
-    return html
+    #uses the template created in templates/index.html
+    return render_template("index.html", templates=templates)
 
 if __name__ == "__main__":
     app.run(debug=True)
