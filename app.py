02034bc9a2bace8d16d6f47d4da2ab9dfdc8e7c5
Yusuf A <yusuf.r.ahmed@proton.me>
2026-09-06 15:21:38 +0100

    setup database connection to flask

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import sqlite3
+
+def connect_db():
+    dbcon = sqlite3.connect("rss.db")
+    dbcon.row_factory = sqlite3.Row #allows acessing by name not just index
+    return dbcon
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<a>Test</a>"
+    dbcon = connect_db()
+    templates = dbcon.execute("SELECT * FROM feed_templates").fetchall()
+    dbcon.close()
+
+    #bulding a html list from db
+    html = "<ul>"
+    for entry in templates:
+        html += f"<li>{entry["name"]} headers = {entry["headers"]}"
+    html += "</ul>"
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
